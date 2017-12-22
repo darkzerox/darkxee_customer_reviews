c160496ff0cdf3f2da3f5ae6914dd014a52ea3b2
Darkzerox <darknesscs2@gmail.com>
2017-12-22 18:27:29 +0700

    update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ Author URI:https://www.darkxee.com
 License: Plugin comes under GPL Licence.
 */
 
-//wp_enqueue_script('custom-js', plugins_url( '/js/script.js' , __FILE__ ) , array( 'jquery' ));
+wp_enqueue_script('custom-js', plugins_url( '/js/script.js' , __FILE__ ) , array( 'jquery' ));
 //wp_localize_script( 'custom-js', 'darkxee', array( 'callurl' => admin_url( 'admin-ajax.php')));
 
 wp_enqueue_style( 'dzx-css', plugins_url('/css/dzx.css', __FILE__) );
@@ -23,13 +23,36 @@ function customer_review($atts){
             'start' => '',
             'end' => '',
             'sku' => '',
-            'orderby' => 'date',           
+            'orderby' => 'star',
+            'sort' => 'DESC',            
+            'limit' => '',
         ), $atts ,'customer_review' 
     );
-    $where = '';   
-    if ($atts['start']!= ''){
-        $where = "WHERE (review_date BETWEEN '".$atts['start']."' AND '".$atts['end']."')";
+    $where = ''; 
+    
+    if ($atts['start']!= '' || $atts['sku']!= '' ){
+        $where = 'WHERE ( ';
+        
+        if ($atts['start']!= ''){
+        $where .= " review_date BETWEEN '".$atts['start']."' AND '".$atts['end']."'";
+        }        
+        if ($atts['sku']!= '' && $atts['start']== '' ){
+            $where .= " product_sku LIKE '".$atts['sku']."' ";
+        }
+        if ($atts['sku']!= '' && $atts['start']!= '' ){
+            $where .= " And product_sku LIKE '".$atts['sku']."' ";
+        }
+
+        $where.=' ) ';
     }
+    $where.= "ORDER BY ".$atts['orderby']." ".$atts['sort'];
+    
+    if ($atts['limit']!= ''){
+          $where .= ' LIMIT '.$atts['limit'];
+    }
+
+    // echo $where;
+    
 
     $datas = $wpdb->get_results("SELECT * FROM wp_customer_review $where");
 
@@ -38,54 +61,70 @@ function customer_review($atts){
     $html .= '<ul class="customer-rev">';
 
     foreach ( $datas as $data ) 
-    {
-       
-        $html .= '<li  id="rev'.$data->id.'">';
-        $html .= '<div class="rev-contener">';
-        $html .= '<div class="rev-img"><a href="'.$data->link.'"><img src="/wp-content/uploads/customer_review/'.$data->img.'"/ alt="'.$data->img_alt.'"></a></div>';
-        $html .= '<h3><a href="'.$data->link.'">'.$data->title.'</a></h3>';  
-        $html .= '<div class="rev-star">';       
-       
+    {    
+        $html .='<li itemprop="review" itemscope itemtype="http://schema.org/Review" id="revId-'.$data->id.'" class="rev-item">';
+        $html .='<div class="rev-contener">';
+        $html .='<div class="rev-img">';
+        $html .='<img itemprop="image" src="/wp-content/uploads/customer_review/'.$data->img.'"/ alt="'.$data->img_alt.'">';
+        $html .='</div>';
+        $html .='<h3 class="rev-title" itemprop="name">'.$data->title.'</h3>';
+        $html .='<h4><span>by</span>';
+        $html .='<span itemprop="author">'.$data->customer_name.'</span></h4>';
+        $html .='<div class="rev-sku">sku: '.$data->product_sku.'</div>';
+        $html .='<meta itemprop="datePublished" content="'.$data->review_date.'">'.$data->review_date;
+        $html .='    <div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">';
+        $html .='            <div class="rev-star">';
+        $html .='            <meta itemprop="worstRating" content="1">';
         for ($i = 0; $i < 5; $i++) {
-            if ($i <= $data->id){
-                $html.= '<span class="fa fa-star getstar"></span>';
-            }else{
-                $html.= '<span class="fa fa-star"></span>';
-            }         
-        }    
-
-        $html .= '</div>';
-        $html .= '<div class="rev-content">'.$data->detail.'</div>';    
-        $html .= '</div>'   ;
+                if ($i < $data->star){ 
+                    $html.= '<span class="fa fa-star getstar"></span>';
+                }else{
+                    $html.= '<span class="fa fa-star"></span>';
+                }         
+            }
+        $html .='           <span itemprop="ratingValue">'.$data->star.'</span>/';
+        $html .='           <span itemprop="bestRating">5</span>stars';
+        $html .='        </div>';
+        $html .='    </div>';
+        $html .='   <span class="rev-detail" itemprop="description">'.$data->detail.'</span>';
+        $html .='</div>';
         $html .='</li>';
+
     }  
     $html.='</ul>';
     echo  $html;
   }
-add_shortcode('dzx_data', 'customer_review');
+add_shortcode('dzx_customer_review', 'customer_review');
+
+add_action('woocommerce_after_single_product','get_product_review_sku');
+function get_product_review_sku(){
+    global $product;
+    echo "<h2>Customer Review</h2>";
+    echo do_shortcode( '[dzx_customer_review sku="'.$product->get_sku().'" limit="4"]' );
+}
 
 
 
 register_activation_hook( __FILE__, 'dzx_plugin_create_db' );
-
 function dzx_plugin_create_db() {
 
 	global $wpdb;	
 
 	$sql = "CREATE TABLE wp_customer_review (
-            id int(20) NOT NULL,
+            id int(20) NOT NULL AUTO_INCREMENT,
             title varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-            img varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-            img_alt varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-            link varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-            detail longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-            star int(10) NOT NULL,
-            product_sku varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-            review_date date NOT NULL
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; ALTER TABLE wp_customer_review
-            ADD PRIMARY KEY (id); ALTER TABLE wp_customer_review
-            MODIFY id int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-            COMMIT; ";
+            customer_name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+            img varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+            img_alt varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+            link varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+            detail longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+            star int(10) NULL DEFAULT 0,
+            product_sku varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+            category varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+            review_date date NULL DEFAULT NULL,
+            priority int(11) NULL DEFAULT 0,
+            PRIMARY KEY  (id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; ";
 
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 	dbDelta( $sql );
--- js/script.js
+++ js/script.js
@@ -1,8 +1,52 @@
-(function($, $window, $document) {
+(function ($, $window, $document) {
+    $.fn.fadeOutAndRemove = function (speed) {
+        $(this).fadeOut(speed, function () {
+            $(this).remove();
+        })
+    }
 
-$(document).ready(function() {
-  
-});
+    $(document).ready(function () {
+        
+        $('.rev-item .rev-img').click(function () {
+            $this = $(this).parent();
+            $('.customer-rev-pop').fadeOutAndRemove('fast');
+
+            var img = $this.find('.rev-img img').attr('src');
+            var title = $this.find('h3').text();
+            var detail = $this.find('.rev-detail').text();
+
+            // console.log(img + title + detail);
+
+            var skele = `            
+            <div class="customer-rev-pop">
+            <div class="customer-rev-shadow"></div>
+                <div class="customer-rev-connenner">
+                    <div class="customer-rev-img">
+                        <img src= "${img}" />
+                    </div>
+                    <div class="customer-rev-title">
+                    ${title}
+                    </div>
+                    <div class="customer-rev-detail">
+                    ${detail}
+                    </div>
+                    <div class="customer-rev-close"><i class="fa fa-times" aria-hidden="true"></i></div>
+                </div>
+            </div>       
+        `;
+        $('body').append($(skele).hide().fadeIn(200));
+
+
+
+            // console.log($this.text());
+
+            $('.customer-rev-close , .customer-rev-shadow').click(function () {
+                $('.customer-rev-pop').fadeOutAndRemove('fast');
+            });
+          
+
+        });
+    });
 
 
 })(jQuery, jQuery(window), jQuery(document));
